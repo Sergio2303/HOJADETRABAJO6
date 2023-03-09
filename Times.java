d64b9b1a17ba2f34dde37e49717fb530d60eab6d
José Gramajo <98564126+abysswalkr@users.noreply.github.com>
2023-03-08 23:43:36 -0600

    Times class

--- Times.java
+++ Times.java
@@ -1,3 +1,17 @@
+import java.util.Map;
+
+/**
+ * The type Times.
+ */
 public class Times {
-    
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        Map MAP = MapBuild.getInitialValues(MapFact.getMap("3"));
+
+    }
 }
